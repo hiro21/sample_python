628beb0e0062b443905d176c152a1609dc7ef93b
hiro21 <hiswing8@gmail.com>
2017-06-27 23:47:57 +0900

    add source

---
+++ multiByte.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+print 'test'
+print 'テスト'
+
+
+def test():
+    print '関数呼び出し'
+
+test()
---
+++ sampleList.py
@@ -0,0 +1,8 @@
+empty_list = []
+print (empty_list)
+
+data1_list = ["test"]
+print (data1_list)
+print (data1_list[0])
+# index error ↓
+# print (data1_list[1])
