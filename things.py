157a64981f69f3269246816a4ac9b4fb1c2f8617
hiro21 <hiswing8@gmail.com>
2017-06-28 00:18:30 +0900

    new

---
+++ things.py
@@ -0,0 +1,12 @@
+things = ["mozzarella", "cinderella", "salmonella"]
+# 人間の要素の先頭文字を大文字に
+things[1] = "Cinderella"
+print (things)
+
+# チーズの種類を大文字に
+things[0] = "Mozzarella"
+print (things)
+
+# 病気のものを削除
+del things[2]
+print (things)
