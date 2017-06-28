8294af2df01265a274991ae39604df8806f66ad1
hiro21 <hiswing8@gmail.com>
2017-06-28 23:36:51 +0900

    upper case add

--- things.py
+++ things.py
@@ -1,10 +1,15 @@
 things = ["mozzarella", "cinderella", "salmonella"]
+
+# リストの中身は書き換えない
+tmp = things[1].capitalize()
+print (tmp)
+
 # 人間の要素の先頭文字を大文字に
 things[1] = "Cinderella"
 print (things)
 
 # チーズの種類を大文字に
-things[0] = "Mozzarella"
+things[0] = things[0].upper()
 print (things)
 
 # 病気のものを削除
