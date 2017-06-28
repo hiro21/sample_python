101d0b470bdb1505d460ad1a3875aeb44708f98a
hiro21 <hiswing8@gmail.com>
2017-06-28 23:46:25 +0900

    first commit

---
+++ surprise.py
@@ -0,0 +1,14 @@
+# make list
+surprise = ["Groucho", "Chico", "Harpo"]
+
+# last word lower
+surprise[-1] = surprise[-1].lower()
+print (surprise)
+
+# reserve
+surprise = surprise[::-1]
+print (surprise)
+
+# top word upper
+surprise[0] = surprise[0].capitalize()
+print (surprise)
