41192c4a27088161da749e4ee1eb2830180d37b5
hiro21 <hiswing8@gmail.com>
2017-09-06 00:10:37 +0900

    sampleIf add conditionnal branch

--- sampleIf.py
+++ sampleIf.py
@@ -1,3 +1,12 @@
 guess_me = 7
-print (guess_me)
 
+# under 7 is 'too low'
+# over 7 is 'too high'
+# equals 7 is 'just right'
+
+if guess_me  == 7 :
+    print ('just right')
+elif guess_me < 7 :
+    print ('too low')
+else:
+    print ('too high')
